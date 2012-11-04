ec097bd77db5523a04ceb15a128e01689d36fb90
Steve Purcell <steve@sanityinc.com>
2012-11-04 19:00:13 +0000

    Update to use flymake-easy

--- flymake-shell.el
+++ flymake-shell.el
@@ -4,6 +4,7 @@
 ;;; Author: Steve Purcell <steve@sanityinc.com>
 ;;; URL: https://github.com/purcell/flymake-shell
 ;;; Version: DEV
+;;; Package-Requires: ((flymake-easy "0.1"))
 ;;;
 ;;; Commentary:
 
@@ -11,57 +12,36 @@
 
 ;;   (require 'flymake-shell)
 ;;   (add-hook 'sh-set-shell-hook 'flymake-shell-load)
-
-(require 'flymake)
+;;
+;; Uses flymake-easy, from https://github.com/purcell/flymake-easy
 
 ;;; Code:
+
+(require 'flymake-easy)
 
 (defconst flymake-shell-supported-shells '(bash zsh sh dash))
 
 (defconst flymake-shell-err-line-pattern-re
-  '(("^\\(.+\\): line \\([0-9]+\\): \\(.+\\)$" 1 2 nil 3) ; bash
+  '(("^\\(.+\\): line \\([0-9]+\\): \\([^`].+\\)$" 1 2 nil 3) ; bash
     ("^\\(.+\\): ?\\([0-9]+\\): \\(.+\\)$" 1 2 nil 3)) ; zsh / dash
   "Regexp matching shell error messages.")
 
-(defun flymake-shell--create-temp-in-system-tempdir (file-name prefix)
-  "Return a temporary file name into which flymake can save buffer contents.
-
-This is tidier than `flymake-create-temp-inplace', and therefore
-preferable when the checking doesn't depend on the file's exact
-location."
-  (make-temp-file (or prefix "flymake-shell") nil ".sh"))
-
-(defun flymake-shell-init ()
+(defun flymake-shell-command (filename)
   "Construct a command that flymake can use to check shell source."
-  (list (symbol-name sh-shell)
-        (list "-n" (flymake-init-create-temp-buffer-copy
-                    'flymake-shell--create-temp-in-system-tempdir))))
+  (list (symbol-name sh-shell) "-n" filename))
 
 ;;;###autoload
 (defun flymake-shell-load ()
-  "Configure flymake mode to check the current buffer's shell-script syntax.
-
-This function is designed to be called in `sh-set-shell-hook'; it
-does not alter flymake's global configuration, so function
-`flymake-mode' alone will not suffice."
+  "Configure flymake mode to check the current buffer's shell-script syntax."
   (interactive)
   (unless (eq 'sh-mode major-mode)
     (error "Cannot enable flymake-shell in this major mode"))
   (if (memq sh-shell flymake-shell-supported-shells)
-      (progn
-        ;; We know we're in shell-script mode, so stop flymake
-        ;; second-guessing based on filename
-        (set (make-local-variable 'flymake-allowed-file-name-masks) '(("." flymake-shell-init)))
-        (set (make-local-variable 'flymake-err-line-patterns) flymake-shell-err-line-pattern-re)
-        (if (executable-find (symbol-name sh-shell))
-            (flymake-mode t)
-          (message "Not enabling flymake: '%s' command not found" sh-shell)))
+      (flymake-easy-load 'flymake-shell-command
+                         flymake-shell-err-line-pattern-re
+                         'tempdir
+                         "sh")
     (message "Shell %s is not supported by flymake-shell" sh-shell)))
-
-
-(defadvice flymake-post-syntax-check (before flymake-force-check-was-interrupted)
-  (setq flymake-check-was-interrupted t))
-(ad-activate 'flymake-post-syntax-check)
 
 
 (provide 'flymake-shell)
